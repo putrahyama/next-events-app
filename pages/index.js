bafb403312f02bafa9d00c2548352b7ac668785d
putrahyama <putracahya.mail@gmail.com>
2023-04-05 13:50:49 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import getFeaturedEvents from '../dummy-data';
+
+const HomePage = () => {
+	const featuredEvents = getFeaturedEvents();
+
+	return (
+		<div>
+			<ul>
+				
+			</ul>
+		</div>
+	);
+};
+
+export default HomePage;
